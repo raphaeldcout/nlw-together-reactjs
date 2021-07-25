07723fd65d385eb17f3016e72bf3dffd7f29428a
Raphael Douglas <rafinha1132009@hotmail.com>
2021-07-25 00:56:12 -0300

    #push modificated

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Home } from './views/Home';
 import { NewRoom } from './views/NewRoom';
 import { Room } from "./views/Room";
+import { AdminRoom } from './views/AdminRoom';
 
 import { AuthContextProvider } from "./contexts/AuthContext";
 
@@ -14,6 +15,8 @@ function App() {
           <Route path='/' exact component={Home} />
           <Route path='/rooms/new' exact component={NewRoom} />
           <Route path='/rooms/:id' component={Room} />
+
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
